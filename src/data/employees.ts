bd81d61ee43f819549e7ac8525410503a48215f4
Kah Kit <kahkitzheng@gmail.com>
2024-09-27 14:54:43 +0200

    Manually generate the list of data

---
+++ src/data/employees.ts
@@ -0,0 +1,164 @@
+export const EMPLOYEES_DATA: Person[] = [
+  {
+    image: "https://7532010.fs1.hubspotusercontent-na1.net/hubfs/7532010/Anouk_2024(Portret)-7(Voorkeur)-1.jpg",
+    name: "Anouk Binkhuysen",
+    firstName: "Anouk",
+    jobTitle: "CEO & Founder"
+  },
+  {
+    image: "https://7532010.fs1.hubspotusercontent-na1.net/hubfs/7532010/Rick.jpg",
+    name: "Rick van Rooij",
+    firstName: "Rick",
+    jobTitle: "CTO"
+  },
+  {
+    image: "https://7532010.fs1.hubspotusercontent-na1.net/hubfs/7532010/Wouter.jpg",
+    name: "Wouter Delken",
+    firstName: "Wouter",
+    jobTitle: "CCO & Co-founder"
+  },
+  {
+    image: "https://7532010.fs1.hubspotusercontent-na1.net/hubfs/7532010/Marloes.jpg",
+    name: "Marloes van Willegen",
+    firstName: "Marloes",
+    jobTitle: "Product Lead Lesmethodes & Opleidingen"
+  },
+  {
+    image: "https://7532010.fs1.hubspotusercontent-na1.net/hubfs/7532010/Lotte-1.jpg",
+    name: "Lotte Steen",
+    firstName: "Lotte",
+    jobTitle: "Product Owner Lesmethodes"
+  },
+  {
+    image: "https://7532010.fs1.hubspotusercontent-na1.net/hubfs/7532010/Sebastien.jpg",
+    name: "Sébastien Leupen",
+    firstName: "Sébastien",
+    jobTitle: "Lead Sales"
+  },
+  {
+    image: "https://7532010.fs1.hubspotusercontent-na1.net/hubfs/7532010/Sanne.jpg",
+    name: "Sanne van Eeuwijk",
+    firstName: "Sanne",
+    jobTitle: "Content Marketeer/Designer"
+  },
+  {
+    image: "https://7532010.fs1.hubspotusercontent-na1.net/hubfs/7532010/Hennie.jpg",
+    name: "Hennie Koelewijn",
+    firstName: "Hennie",
+    jobTitle: "Customer Experience Researcher"
+  },
+  {
+    image: "https://7532010.fs1.hubspotusercontent-na1.net/hubfs/7532010/Danielle-3(voorkeur)-1.jpg",
+    name: "Danielle van Gessel",
+    firstName: "Danielle",
+    jobTitle: "Product Owner Platform"
+  },
+  {
+    image: "https://7532010.fs1.hubspotusercontent-na1.net/hubfs/7532010/Patricia.jpg",
+    name: "Patricia van Ark",
+    firstName: "Patricia",
+    jobTitle: "People & Culture Manager"
+  },
+  {
+    image: "https://7532010.fs1.hubspotusercontent-na1.net/hubfs/7532010/Anke.jpg",
+    name: "Anke Govers",
+    firstName: "Anke",
+    jobTitle: "Lead Content"
+  },
+  {
+    image: "https://7532010.fs1.hubspotusercontent-na1.net/hubfs/7532010/Jochen-1.jpg",
+    name: "Jochen Schoofs",
+    firstName: "Jochen",
+    jobTitle: "Copywriter"
+  },
+  {
+    image: "https://7532010.fs1.hubspotusercontent-na1.net/hubfs/7532010/Elvira.jpg",
+    name: "Elvira Bozileva",
+    firstName: "Elvira",
+    jobTitle: "Back End Developer"
+  },
+  {
+    image: "https://7532010.fs1.hubspotusercontent-na1.net/hubfs/7532010/Bram-6-1.jpg",
+    name: "Bram van de Werdt",
+    firstName: "Bram",
+    jobTitle: "Adviseur"
+  },
+  {
+    image: "https://7532010.fs1.hubspotusercontent-na1.net/hubfs/7532010/Kahkit-4-1.jpg",
+    name: "Kah Kit Zheng",
+    firstName: "Kah Kit",
+    jobTitle: "Front End Developer"
+  },
+  {
+    image: "https://7532010.fs1.hubspotusercontent-na1.net/hubfs/7532010/Tim-5-1.jpg",
+    name: "Tim Jansen",
+    firstName: "Tim",
+    jobTitle: "Content Developer"
+  },
+  {
+    image: "https://7532010.fs1.hubspotusercontent-na1.net/hubfs/7532010/Janou-3(voorkeur)-1.jpg",
+    name: "Janou Valkenburg",
+    firstName: "Janou",
+    jobTitle: "Content Developer"
+  },
+  {
+    image: "https://7532010.fs1.hubspotusercontent-na1.net/hubfs/7532010/Hilde-4(voorkeur)-1.jpg",
+    name: "Hilde Zendman",
+    firstName: "Hilde",
+    jobTitle: "Content Developer"
+  },
+  {
+    image: "https://7532010.fs1.hubspotusercontent-na1.net/hubfs/7532010/Linne(voorkeur)-1.jpg",
+    name: "Linne Polman",
+    firstName: "Linne",
+    jobTitle: "Content Developer"
+  },
+  {
+    image: "https://7532010.fs1.hubspotusercontent-na1.net/hubfs/7532010/Britta-2-1.jpg",
+    name: "Britta Zonderland",
+    firstName: "Britta",
+    jobTitle: "Analist"
+  },
+  {
+    image: "https://7532010.fs1.hubspotusercontent-na1.net/hubfs/7532010/Jordy-6(Voorkeur)-1-1.jpg",
+    name: "Jordy Katoele",
+    firstName: "Jordy",
+    jobTitle: "Adviseur"
+  },
+  {
+    image: "https://7532010.fs1.hubspotusercontent-na1.net/hubfs/7532010/Max-3(Voorkeur)-1.jpeg",
+    name: "Max Wognum",
+    firstName: "Max",
+    jobTitle: "UI Developer"
+  },
+  {
+    image: "https://7532010.fs1.hubspotusercontent-na1.net/hubfs/7532010/SannevanderLaan-3-2.jpg",
+    name: "Sanne van der Laan",
+    firstName: "Sanne",
+    jobTitle: "DTP'er"
+  },
+  {
+    image: "https://7532010.fs1.hubspotusercontent-na1.net/hubfs/7532010/Chloe-4(Voorkeur)-1.jpg",
+    name: "Chloé Kasto",
+    firstName: "Chloé",
+    jobTitle: "Coördinerend Vormgever"
+  },
+  {
+    image: "https://7532010.fs1.hubspotusercontent-na1.net/hubfs/7532010/Lorena-3(Voorkeur)-1.jpg",
+    name: "Lorena Marchesin",
+    firstName: "Lorena",
+    jobTitle: "Lead Development"
+  },
+  {
+    image: "https://7532010.fs1.hubspotusercontent-na1.net/hubfs/7532010/Eva-12-1.jpeg",
+    name: "Eva van de Burgt",
+    firstName: "Eva",
+    jobTitle: "Customer Success Specialist"
+  },
+  {
+    image: "https://7532010.fs1.hubspotusercontent-na1.net/hubfs/7532010/Iris-4(Voorkeur)-1.jpg",
+    name: "Iris van den Breemer",
+    firstName: "Iris",
+    jobTitle: "Customer Success Specialist"
+  }
+];
